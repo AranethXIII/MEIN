133c1ad1a2ef419a9b9c73322f22d4c3059f7fdd
Fabian Schulz <fabian.schulz2@hotmail.de>
2019-06-24 11:22:45 +0200

    Mockup

---
+++ app/src/main/java/com/example/myapplication/View/Game.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.myapplication.R;
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
