c988d6b7c3dc721d3a88b69444a904597a9804e6
Fabian Schulz <fabian.schulz2@hotmail.de>
2019-07-13 20:24:55 +0200

    GUI ueberarbeitet

--- app/src/main/java/com/example/myapplication/View/MainActivity.java
+++ app/src/main/java/com/example/myapplication/View/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
@@ -11,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = findViewById(R.id.start_game);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Game.class));
+            }
+        });
     }
 }
