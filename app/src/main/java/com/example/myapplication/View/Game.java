0f68a72e686595f33cb93d6e83042f3d2ecdf66a
pjilge <pjilge@web.de>
2019-07-15 13:09:39 +0200

    speech

--- app/src/main/java/com/example/myapplication/View/Game.java
+++ app/src/main/java/com/example/myapplication/View/Game.java
@@ -2,6 +2,8 @@ package com.example.myapplication.View;
 
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,6 +15,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.Model.Scenario;
 
 import com.example.myapplication.R;
+
+import java.util.Locale;
 
 
 public class Game extends AppCompatActivity {
@@ -26,7 +30,7 @@ public class Game extends AppCompatActivity {
     Button button;
     Scenario scenario;
     private boolean done;
-
+    TextToSpeech mTTS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,96 +42,87 @@ public class Game extends AppCompatActivity {
         aw4 = findViewById(R.id.aw4);
         tex = findViewById(R.id.textView2);
         button = findViewById(R.id.next);
-        done =false;
+        done = false;
 
         //TODO use Model.Scenario class for backend interaction
 
         //=======example
-
-        scenario = Scenario.randomScenario();
-        button.setOnClickListener(new View.OnClickListener() {
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
-            public void onClick(View view) {
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.GERMAN);
 
-                if(done){
-                    scenario=Scenario.randomScenario();
-                    System.out.println("---------new Scenario------------");
-                    fillField();
-                    done=false;
-                }
-                if (radioGroup.getCheckedRadioButtonId() != -1) {
-                    int ans=-1;
-                    if(aw1.isChecked()) ans=0;
-                    if(aw2.isChecked()) ans=1;
-                    if(aw3.isChecked()) ans=2;
-                    if(aw4.isChecked()) ans=3;
-                    scenario.selectAction(ans);
-
-                    if (scenario.getAnsweroptions()==null) {
-                        tex.setText(scenario.getcurrentState());
-                        aw1.setVisibility(View.INVISIBLE);
-                        aw2.setVisibility(View.INVISIBLE);
-                        aw3.setVisibility(View.INVISIBLE);
-                        aw4.setVisibility(View.INVISIBLE);
-                        done=true;
-
-                    }else{
-                        fillField();
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    } else {
+                        Log.e("TTS", "Initialization failed");
                     }
                 }
             }
         });
 
+        scenario = Scenario.randomScenario();
+//        mTTS.speak(scenario.getcurrentDescription(),TextToSpeech.QUEUE_ADD,null);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (done) {
+                    scenario = Scenario.randomScenario();
+                    System.out.println("---------new Scenario------------");
+                    fillField();
+                    done = false;
+                }
+                if (radioGroup.getCheckedRadioButtonId() != -1) {
+                    int ans = -1;
+                    if (aw1.isChecked()) ans = 0;
+                    if (aw2.isChecked()) ans = 1;
+                    if (aw3.isChecked()) ans = 2;
+                    if (aw4.isChecked()) ans = 3;
+                    scenario.selectAction(ans);
+
+                    if (scenario.getAnsweroptions() == null) {
+                        tex.setText(scenario.getcurrentState());
+                        aw1.setVisibility(View.INVISIBLE);
+                        aw2.setVisibility(View.INVISIBLE);
+                        aw3.setVisibility(View.INVISIBLE);
+                        aw4.setVisibility(View.INVISIBLE);
+                        done = true;
+                        mTTS.speak(scenario.getcurrentState(),TextToSpeech.QUEUE_FLUSH,null);
+                    } else {
+                        fillField();
+                    }
+                }
+            }
+        });
+        mTTS.speak("Schinken",TextToSpeech.QUEUE_ADD,null);
+
+
+
         fillField();
-/*
-        Scenario s1 = Scenario.randomScenario();
-
-        System.out.println(s1.getVcase());
-
-        System.out.println(s1.getAnsweroptions());
-
-        System.out.println("select option 1");
-        s1.selectAction(0);
-
-        System.out.println(s1.getcurrentDescription());
-        System.out.println(s1.getcurrentState());
-        System.out.println(s1.getAnsweroptions());
-
-
-        System.out.println();
-
-        System.out.println("Scenario 2");
-
-        Scenario s2 = Scenario.randomScenario();
-
-        System.out.println(s2.getVcase());
-
-        System.out.println(s2.getAnswerOptionDescriptions());
-
-        System.out.println("select option 3");
-        s2.selectAction(2);
-
-        System.out.println(s2.getcurrentDescription());
-        System.out.println(s2.getcurrentState());
-        System.out.println(s2.getAnswerOptionDescriptions());
-
-        System.out.println("select option 1");
-        s2.selectAction(0);
-
-        System.out.println(s2.getcurrentDescription());
-        System.out.println(s2.getcurrentState());
-        System.out.println(s2.getAnswerOptionDescriptions());
-
-*/
-        //=======
     }
+        @Override
+        protected void onDestroy() {
+        super.onDestroy();
+            if (mTTS != null) {
+                mTTS.stop();
+                mTTS.shutdown();
+            }
+        }
+
 
     void fillField() {
         tex.setText(scenario.getVcase().getDescription());
+
+        mTTS.speak(scenario.getVcase().getDescription(),TextToSpeech.QUEUE_FLUSH,null);
         aw1.setText(scenario.getAnswerOptionDescriptions().get(0));
         aw2.setText(scenario.getAnswerOptionDescriptions().get(1));
         aw3.setText(scenario.getAnswerOptionDescriptions().get(2));
         aw4.setText(scenario.getAnswerOptionDescriptions().get(3));
+
 
 
         aw1.setVisibility(View.VISIBLE);
@@ -139,5 +134,15 @@ public class Game extends AppCompatActivity {
         aw2.setChecked(false);
         aw3.setChecked(false);
         aw4.setChecked(false);
+    }
+
+    private void speak(String s) {
+        String text = s;
+        float pitch=0.1f;
+        float speed=0.1f;
+        mTTS.setPitch(0.1f);
+        mTTS.setSpeechRate(0.1f);
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
